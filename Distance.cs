5b29ec8a7fa173c8daecc0040d6a48009cae1455
Katrine-GitH <119107912+Katrine-GitH@users.noreply.github.com>
2022-12-11 13:43:04 +0300

    Add files via upload

---
+++ Distance.cs
@@ -0,0 +1,33 @@
+﻿// Задача 21 Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 2D пространстве.
+//A (0,0); B (1,1), -> ~1.4
+//A (2,4); B (0,7) -> ~3.6
+
+Console.WriteLine("Напишите координаты x1 ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($" x1= {x1}");
+Console.WriteLine("Напишите координаты y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($" y1={y1}");
+Console.WriteLine("Напишите координаты x2 ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($" x2= {x2}");
+Console.WriteLine("Напишите координаты y1");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($" y2={y2}");
+double Distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+Console.WriteLine($"Расстояние между двумя точками равно:{Distance}");
+
+
+
+
+
+//Console.Write("Введите имя: ");
+//string? name = Console.ReadLine();
+//Console.Write("Введите возраст: ");
+//int age = Convert.ToInt32(Console.ReadLine());
+//Console.Write("Введите рост: ");
+//double height = Convert.ToDouble(Console.ReadLine());
+//Console.Write("Введите размер зарплаты: ");
+//decimal salary = Convert.ToDecimal(Console.ReadLine());
+//Console.WriteLine($"Имя: {name}  Возраст: {age}  Рост: {height}м  Зарплата: {salary}$");
---
+++ TableCube.cs
@@ -0,0 +1,13 @@
+﻿//Задача 23 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.WriteLine("Напишите число N");
+double number = 1;
+double N = Convert.ToDouble(Console.ReadLine());
+double Cube = 0;
+while (number <= N)
+{
+    Cube = number * number * number;
+    Console.WriteLine($"{number}^3={Cube} ");
+    number++;
+}
